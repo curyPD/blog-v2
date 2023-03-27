e92cd7fbab37c915aa6b3aa8a7de5de1af04fa97
curyPD <romandruzhinin2003@gmail.com>
2023-03-27 12:09:31 +0300

    Add new font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Open_Sans } from "next/font/google";
 import UserDataProvider from "@/context/UserDataProvider";
 import Layout from "@/components/Layout";
 import DashboardProvider from "@/context/DashboardProvider";
+
+const openSans = Open_Sans({
+    subsets: ["latin"],
+    variable: "--font-open-sans",
+    display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <UserDataProvider>
             <DashboardProvider>
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
+                <div className={`${openSans.variable} font-sans`}>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </div>
             </DashboardProvider>
         </UserDataProvider>
     );
