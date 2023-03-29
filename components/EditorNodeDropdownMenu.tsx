0051b0a1cbcfaf6c7176d7b4b733ef36093ec44c
curyPD <romandruzhinin2003@gmail.com>
2023-03-29 19:19:20 +0300

    Create a node dropdown for editor menu

---
+++ components/EditorNodeDropdownMenu.tsx
@@ -0,0 +1,150 @@
+import { useState } from "react";
+import { useDashboard } from "@/context/DashboardProvider";
+import { HiOutlineChevronUpDown } from "react-icons/hi2";
+
+type NodeType =
+    | "Normal"
+    | "Heading 1"
+    | "Heading 2"
+    | "Heading 3"
+    | "Heading 4"
+    | "Heading 5"
+    | "Heading 6";
+
+export default function EditorNodeDropdownMenu() {
+    const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+    const [selectedNode, setSelectedNode] = useState<NodeType>("Normal");
+
+    const { editor } = useDashboard();
+    return (
+        <div className="relative">
+            <button
+                onClick={() => setDropdownOpen((prevState) => !prevState)}
+                className="flex items-center gap-3 border-r border-zinc-400 py-1 pl-3 pr-1 text-sm font-semibold text-zinc-600"
+            >
+                <span>{selectedNode}</span>
+                <HiOutlineChevronUpDown className="h-5 w-5 text-zinc-500" />
+            </button>
+            {dropdownOpen && (
+                <div className="absolute top-full z-10 flex max-h-56 w-52 -translate-y-1 translate-x-2 flex-col overflow-x-scroll rounded border border-zinc-300 bg-white p-1 shadow-lg">
+                    <button
+                        className={`rounded-sm px-3 py-2 text-left text-sm font-normal ${
+                            editor?.isActive("paragraph")
+                                ? "bg-blue-100 text-blue-500"
+                                : "bg-white text-zinc-600"
+                        }`}
+                        onClick={() => {
+                            setSelectedNode("Normal");
+
+                            editor?.chain().focus().setParagraph().run();
+                        }}
+                    >
+                        Normal
+                    </button>
+                    <button
+                        className={`rounded-sm px-3 py-2 text-left text-3xl font-bold ${
+                            editor?.isActive("heading", { level: 1 })
+                                ? "bg-blue-100 text-blue-500"
+                                : "bg-white text-zinc-600"
+                        }`}
+                        onClick={() => {
+                            setSelectedNode("Heading 1");
+                            editor
+                                ?.chain()
+                                .focus()
+                                .toggleHeading({ level: 1 })
+                                .run();
+                        }}
+                    >
+                        Heading 1
+                    </button>
+                    <button
+                        className={`rounded-sm px-3 py-2 text-left text-2xl font-bold ${
+                            editor?.isActive("heading", { level: 2 })
+                                ? "bg-blue-100 text-blue-500"
+                                : "bg-white text-zinc-600"
+                        }`}
+                        onClick={() => {
+                            setSelectedNode("Heading 2");
+                            editor
+                                ?.chain()
+                                .focus()
+                                .toggleHeading({ level: 2 })
+                                .run();
+                        }}
+                    >
+                        Heading 2
+                    </button>
+                    <button
+                        className={`rounded-sm px-3 py-2 text-left text-xl font-bold ${
+                            editor?.isActive("heading", { level: 3 })
+                                ? "bg-blue-100 text-blue-500"
+                                : "bg-white text-zinc-600"
+                        }`}
+                        onClick={() => {
+                            setSelectedNode("Heading 3");
+                            editor
+                                ?.chain()
+                                .focus()
+                                .toggleHeading({ level: 3 })
+                                .run();
+                        }}
+                    >
+                        Heading 3
+                    </button>
+                    <button
+                        className={`rounded-sm px-3 py-2 text-left text-lg font-bold ${
+                            editor?.isActive("heading", { level: 4 })
+                                ? "bg-blue-100 text-blue-500"
+                                : "bg-white text-zinc-600"
+                        }`}
+                        onClick={() => {
+                            setSelectedNode("Heading 4");
+                            editor
+                                ?.chain()
+                                .focus()
+                                .toggleHeading({ level: 4 })
+                                .run();
+                        }}
+                    >
+                        Heading 4
+                    </button>
+                    <button
+                        className={`rounded-sm px-3 py-2 text-left text-base font-bold ${
+                            editor?.isActive("heading", { level: 5 })
+                                ? "bg-blue-100 text-blue-500"
+                                : "bg-white text-zinc-600"
+                        }`}
+                        onClick={() => {
+                            setSelectedNode("Heading 5");
+                            editor
+                                ?.chain()
+                                .focus()
+                                .toggleHeading({ level: 5 })
+                                .run();
+                        }}
+                    >
+                        Heading 5
+                    </button>
+                    <button
+                        className={`rounded-sm px-3 py-2 text-left text-sm font-bold ${
+                            editor?.isActive("heading", { level: 6 })
+                                ? "bg-blue-100 text-blue-500"
+                                : "bg-white text-zinc-600"
+                        }`}
+                        onClick={() => {
+                            setSelectedNode("Heading 6");
+                            editor
+                                ?.chain()
+                                .focus()
+                                .toggleHeading({ level: 6 })
+                                .run();
+                        }}
+                    >
+                        Heading 6
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+}
--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,5 +1,7 @@
-import { EditorContent, Editor } from "@tiptap/react";
 import { useCallback } from "react";
+import { EditorContent, Editor } from "@tiptap/react";
+import Label from "./Label";
+import EditorNodeDropdownMenu from "./EditorNodeDropdownMenu";
 
 type TextEditorProps = {
     editor: Editor | null;
@@ -8,8 +10,8 @@ type TextEditorProps = {
 export default function TextEditor({ editor }: TextEditorProps) {
     const setLink = useCallback(() => {
         if (!editor) return;
-        const previousUrl = editor.getAttributes("link").href;
-        const url = window.prompt("URL", previousUrl);
+        const previousUrl: string = editor.getAttributes("link").href;
+        const url: string | null = window.prompt("URL", previousUrl);
 
         // cancelled
         if (url === null) {
@@ -34,140 +36,81 @@ export default function TextEditor({ editor }: TextEditorProps) {
 
     return (
         <div>
-            <div className="flex gap-1 p-1 flex-wrap">
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() => editor?.chain().focus().toggleBold().run()}
-                >
-                    Bold
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() => editor?.chain().focus().toggleItalic().run()}
-                >
-                    Italic
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() => editor?.chain().focus().toggleStrike().run()}
-                >
-                    Strike
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() => editor?.chain().focus().setParagraph().run()}
-                >
-                    Paragraph
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() =>
-                        editor?.chain().focus().toggleBulletList().run()
-                    }
-                >
-                    Bullet List
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() =>
-                        editor?.chain().focus().toggleOrderedList().run()
-                    }
-                >
-                    Ordered List
-                </button>
-                <button
-                    onClick={setLink}
-                    className="p-0.5 border border-black rounded-sm"
-                >
-                    setLink
-                </button>
-                <button
-                    onClick={() => editor?.chain().focus().unsetLink().run()}
-                    disabled={!editor?.isActive("link")}
-                    className="p-0.5 border border-black rounded-sm"
-                >
-                    unsetLink
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() => editor?.chain().focus().setHardBreak().run()}
-                >
-                    Line Break
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() =>
-                        editor
-                            ?.chain()
-                            .focus()
-                            .toggleHeading({ level: 1 })
-                            .run()
-                    }
-                >
-                    H1
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() =>
-                        editor
-                            ?.chain()
-                            .focus()
-                            .toggleHeading({ level: 2 })
-                            .run()
-                    }
-                >
-                    H2
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() =>
-                        editor
-                            ?.chain()
-                            .focus()
-                            .toggleHeading({ level: 3 })
-                            .run()
-                    }
-                >
-                    H3
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() =>
-                        editor
-                            ?.chain()
-                            .focus()
-                            .toggleHeading({ level: 4 })
-                            .run()
-                    }
-                >
-                    H4
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() =>
-                        editor
-                            ?.chain()
-                            .focus()
-                            .toggleHeading({ level: 5 })
-                            .run()
-                    }
-                >
-                    H5
-                </button>
-                <button
-                    className="p-0.5 border border-black rounded-sm"
-                    onClick={() =>
-                        editor
-                            ?.chain()
-                            .focus()
-                            .toggleHeading({ level: 6 })
-                            .run()
-                    }
-                >
-                    H6
-                </button>
+            <Label
+                text="content"
+                isLabelTag={false}
+                onClick={() => editor?.commands.focus()}
+            />
+            <div className="rounded-sm border border-zinc-400">
+                <div className="flex">
+                    <EditorNodeDropdownMenu />
+                    {/* <button
+                        className="rounded-sm border border-black p-0.5"
+                        onClick={() =>
+                            editor?.chain().focus().toggleBold().run()
+                        }
+                    >
+                        Bold
+                    </button>
+                    <button
+                        className="rounded-sm border border-black p-0.5"
+                        onClick={() =>
+                            editor?.chain().focus().toggleItalic().run()
+                        }
+                    >
+                        Italic
+                    </button>
+                    <button
+                        className="rounded-sm border border-black p-0.5"
+                        onClick={() =>
+                            editor?.chain().focus().toggleStrike().run()
+                        }
+                    >
+                        Strike
+                    </button>
+                    
+                    <button
+                        className="rounded-sm border border-black p-0.5"
+                        onClick={() =>
+                            editor?.chain().focus().toggleBulletList().run()
+                        }
+                    >
+                        Bullet List
+                    </button>
+                    <button
+                        className="rounded-sm border border-black p-0.5"
+                        onClick={() =>
+                            editor?.chain().focus().toggleOrderedList().run()
+                        }
+                    >
+                        Ordered List
+                    </button>
+                    <button
+                        onClick={setLink}
+                        className="rounded-sm border border-black p-0.5"
+                    >
+                        setLink
+                    </button>
+                    <button
+                        onClick={() =>
+                            editor?.chain().focus().unsetLink().run()
+                        }
+                        disabled={!editor?.isActive("link")}
+                        className="rounded-sm border border-black p-0.5"
+                    >
+                        unsetLink
+                    </button>
+                    <button
+                        className="rounded-sm border border-black p-0.5"
+                        onClick={() =>
+                            editor?.chain().focus().setHardBreak().run()
+                        }
+                    >
+                        Line Break
+                    </button> */}
+                </div>
+                <EditorContent editor={editor} />
             </div>
-            <EditorContent editor={editor} />
         </div>
     );
 }
