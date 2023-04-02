a05abd71e32ed73787dbd2ccb41f337e86ad0024
curyPD <romandruzhinin2003@gmail.com>
2023-04-02 12:22:19 +0300

    Create a footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    const now: Date = new Date();
+    const year: number = now.getFullYear();
+
+    return (
+        <footer className="border-t border-zinc-200 bg-white">
+            <div className="container mx-auto flex h-12 items-center px-4 text-sm text-zinc-400 xs:px-6 lg:h-14 lg:px-9 lg:text-base xl:max-w-screen-xl 2xl:px-0">
+                Copyright &copy; {year} by Roman Druzhinin.
+            </div>
+        </footer>
+    );
+}
