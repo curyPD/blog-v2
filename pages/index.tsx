381c953ca920fcdd53919a46ca101a4feb955fca
curyPD <romandruzhinin2003@gmail.com>
2023-04-02 12:23:13 +0300

    Style index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
-import Link from "next/link";
 import { GetStaticProps } from "next";
+
+import ArticleCard from "@/components/ArticleCard";
+import Footer from "@/components/Footer";
 
 // import { db } from "@/firebase/firebase";
 // import { get, ref, child } from "firebase/database";
@@ -18,22 +20,35 @@ export default function Home({ articles }: { articles: ArticleType[] }) {
                     content="width=device-width, initial-scale=1"
                 />
             </Head>
-            <main>
-                <h1 className="text-slate-900 text-3xl font-bold">
-                    Polyglot Dream
-                </h1>
-                <div className="flex gap-4">
-                    {articles.length &&
-                        articles.map((article) => (
-                            <article key={article.id}>
-                                <Link href={`/articles/${article.id}`}>
-                                    <img src={article.image} alt="" />
-                                    <h2>{article.title}</h2>
-                                </Link>
-                            </article>
-                        ))}
+            <main className="pt-14 pb-24">
+                <div className="container mx-auto px-4 xs:px-6 lg:px-9 xl:max-w-screen-xl 2xl:px-0">
+                    <div className="mb-14 flex flex-col items-center gap-6 md:flex-row md:justify-between">
+                        <h1 className="text-center text-5xl font-extrabold tracking-tighter text-zinc-900 lg:text-7xl">
+                            Polyglot Dream
+                        </h1>
+                        <p className="text-center text-base text-zinc-600 lg:text-lg">
+                            A blog about language learning.
+                        </p>
+                    </div>
+                    {articles.length ? (
+                        <div className="grid grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-12 md:gap-y-20">
+                            {articles.map((article) => (
+                                <ArticleCard
+                                    key={article.id}
+                                    id={article.id}
+                                    title={article.title}
+                                    imageMd={article.imageMd}
+                                    content={article.content}
+                                    created={article.created}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <div>Nothing here...</div>
+                    )}
                 </div>
             </main>
+            <Footer />
         </>
     );
 }
