f2991a61a2dd2794ba4a92b40fae0c0fa87c6838
curyPD <romandruzhinin2003@gmail.com>
2023-04-08 14:22:05 +0300

    Make h1 on home page bigger

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,13 +21,14 @@ export default function Home({ articles }: { articles: ArticleType[] }) {
             </Head>
             <main className="pt-14 pb-24">
                 <div className="container mx-auto px-4 xs:px-6 lg:px-9 xl:max-w-screen-xl 2xl:px-0">
-                    <div className="mb-16 lg:mb-24">
-                        <h1 className="mb-7 text-4xl font-extrabold tracking-tight text-zinc-900 lg:mb-12 lg:text-7xl lg:leading-[1.1]">
+                    <div className="mb-16 lg:mb-24 xl:mb-32">
+                        <h1 className="mb-7 text-4xl font-extrabold tracking-tight text-zinc-900 lg:mb-12 lg:text-7xl lg:leading-[1.1] xl:mb-20 xl:text-9xl xl:font-black">
                             Hop on, it's time to learn a foreign language.
                         </h1>
-                        <p className="text-lg text-zinc-600 lg:text-2xl">
+                        <p className="text-lg text-zinc-600 lg:text-xl xl:text-2xl xl:leading-normal">
                             Hi, my name is Roman. Here I share what I know about
-                            language learning. Twice a year, maybe?
+                            language learning.
+                            <br /> Twice a year, maybe?
                         </p>
                     </div>
                     {articles.length ? (
